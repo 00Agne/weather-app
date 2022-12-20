d4c8fe4c01941b68f7092434be1edf5b2680e522
00Agne <agne.serksniene@mail.com>
2022-12-20 15:07:25 +0100

    add js

---
+++ src/index.js
@@ -0,0 +1,68 @@
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[dayIndex];
+
+  return `${day} ${hours}:${minutes}`;
+}
+let timeElement = document.querySelector("#today");
+let currentTime = new Date();
+timeElement.innerHTML = formatDate(currentTime);
+
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+}
+
+function searchCity(city) {
+  let apiKey = "b6ede4e3c36a09233349131078c15f46";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+function showCurrentLocTemp(position) {
+  let apiKey = "b6ede4e3c36a09233349131078c15f46";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function showCurrentPosition(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showCurrentLocTemp);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+let button = document.querySelector("#current");
+button.addEventListener("click", showCurrentPosition);
+
+searchCity("Kaunas");
